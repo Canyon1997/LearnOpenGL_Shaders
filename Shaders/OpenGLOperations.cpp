0261bfdf7f5a449eb0917840db64f781ab05cad2
Canyon1997 <canyon.davidson@gmail.com>
2024-08-13 21:49:05 -0400

    initial setup of OpenGL window for shader lesson

---
+++ Shaders/OpenGLOperations.cpp
@@ -0,0 +1,36 @@
+#include "OpenGLOperations.h"
+#include <iostream>
+
+
+GLFWwindow* InitializeOpenGL()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Shaders", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        exit(-1);
+    }
+    glfwMakeContextCurrent(window);
+
+    glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to load GLAD" << std::endl;
+        glfwTerminate();
+        exit(-1);
+    }
+
+    return window;
+}
+
+void FrameBufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
---
+++ Shaders/main.cpp
@@ -0,0 +1,6 @@
+#include "OpenGLOperations.h"
+
+void main()
+{
+
+}
